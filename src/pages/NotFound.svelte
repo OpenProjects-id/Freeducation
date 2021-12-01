6a9ab8539f86d0b5a74c13375cf23369a0883a7a
algonacci <algonacci1@gmail.com>
2021-12-02 01:19:03 +0700

    feat: Add the SPA for donation detail and not-found
    Masih ada bug di dalamnya, akan masuk nanti ke branch hotfix untuk diperbaiki

---
+++ src/pages/NotFound.svelte
@@ -0,0 +1,27 @@
+<!-- notfound section -->
+<section class="waypoint-tigger xs-section-padding">
+    <div class="container">
+      <div class="xs-heading row xs-mb-60 justify-content-center text-center">
+        <div class="col-md-6 col-xl-6">
+          <img
+            src="/assets/images/page-not-found.png"
+            alt=""
+            width="300"
+            height="300" />
+          <h2 class="xs-title">Oops!</h2>
+          <p>Ada sesuatu yang salah, kami tidak bisa menemukan halaman tersebut.</p>
+          <a href="/" class="btn btn-primary mt-4">Kembali ke Beranda</a>
+        </div>
+        <!-- .xs-heading-title END -->
+      </div>
+      <!-- .row end -->
+    </div>
+    <!-- .container end -->
+  </section>
+  <!-- End notfound section -->
+
+  <style>
+      section {
+          max-height: 100vh;
+      }
+  </style>
